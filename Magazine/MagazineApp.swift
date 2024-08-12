b5383c4117438475899c69041a60af110ba0efb7
Avrutin Vlad <avrutin1991@gmail.com>
2024-08-12 11:22:36 +0300

    Initial Commit

---
+++ Magazine/MagazineApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagazineApp.swift
+//  Magazine
+//
+//  Created by Vladislav Avrutin on 12.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct MagazineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
